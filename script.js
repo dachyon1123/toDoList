c0cfbe11f6f3a3e8bb8251dc6e1bebe28570cf0a
Dylan Polito <dpolito1123@gmail.com>
2023-09-25 15:40:02 -0500

    Added functionality to the code.

--- script.js
+++ script.js
@@ -7,8 +7,26 @@ let amountOfItems = 0;
 
 function createListItem() {
   var listItem = document.createElement('li');
+  var deleteButtonDiv = document.createElement('div');
+  var deleteButton = document.createElement('button');
+
+  deleteButtonDiv.setAttribute("id", "deleteButtonDiv" + amountOfItems);
+  deleteButtonDiv.style.display = "flex";
+  deleteButtonDiv.style.justifyContent = "space-between";
+  deleteButtonDiv.style.gap = "10px";
+
+
+  deleteButton.setAttribute("id", "deleteButton" + amountOfItems);
+  deleteButton.textContent="X"
+
+  deleteButton.addEventListener('click', function() {
+    deleteButton.parentElement.remove();
+  })
+
   listItem.textContent= listInput.value
-  list.appendChild(listItem);
+  list.appendChild(deleteButtonDiv);
+  deleteButtonDiv.appendChild(listItem);
+  deleteButtonDiv.appendChild(deleteButton)
   listInput.value = '';
   amountOfItems += 1;
 }
@@ -20,6 +38,8 @@ function clearList() {
 
 
 
+
 btn1.addEventListener('click', createListItem);
 
 btn2.addEventListener('click', clearList)
+
